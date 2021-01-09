5c42b904eec20dfe19d5eecb6a8ab9a90c787e43
Michael Galkin <chivinavy@gmail.com>
2021-01-09 15:09:29 +1000

    Init commit.

---
+++ Crypto/Crypto.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "RUS");
+
+    char buff[50] = { 0 };
+
+    cout << "Введите сообщение прописными латинскими буквами,которое будет закодированно" << endl;
+    cin >> buff;
+
+    for (int i = 0; i < strlen(buff); i++)
+    {
+         buff[i] += 3;
+         if (buff[i] > 122)
+         {
+             buff[i] -= 26;
+         }
+    }
+      
+            
+         
+    cout << buff << endl;
+
+    cout << "Decode: " << endl;
+    for (int i = 0; i < strlen(buff); i++)
+    {
+        buff[i] -= 3;
+        if (buff[i] < 97)
+        {
+            buff[i] += 26;
+        }
+    }
+    
+    cout << buff << endl;
+
+    system("pause");
+    return 0;
+}
